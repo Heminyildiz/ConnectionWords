20204c04f6b470d087c81be66e114abf052c3193
H.Emin Yıldız <46842174+Heminyildiz@users.noreply.github.com>
2025-02-15 18:52:22 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="py-2 bg-transparent">
-      <div className="flex items-center justify-center">
-        {/* Küçük belge/text simgesi */}
+    <header className="py-2 flex items-center justify-center">
+      <div className="flex items-center">
         <svg 
-          className="h-6 w-6 text-gray-700 mr-2" 
+          className="h-5 w-5 text-gray-700 mr-1" 
           xmlns="http://www.w3.org/2000/svg" 
-          fill="none" 
-          viewBox="0 0 24 24" 
-          stroke="currentColor"
+          fill="none" viewBox="0 0 24 24" stroke="currentColor"
         >
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 7h10M7 11h10M7 15h10M5 3h14a2 2 0 012 2v14a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2z" />
         </svg>
@@ -21,6 +18,7 @@ const Header = () => {
 };
 
 export default Header;
+
 
 
 
