3b8ebb2fba45e655c9b792de7519a784808a998f
H.Emin Yıldız <46842174+Heminyildiz@users.noreply.github.com>
2025-02-15 12:29:54 +0300

    Create index.js

---
+++ src/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ConnectionWords from "./ConnectionWords";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ConnectionWords />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
