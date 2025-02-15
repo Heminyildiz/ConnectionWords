e6a86a15af1a6218bf3326ed075618d9e35e6ab2
H.Emin Yıldız <46842174+Heminyildiz@users.noreply.github.com>
2025-02-15 16:43:39 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Header = ({ errorCount, errorLimit, gameStatus }) => {
   return (
-    <header className="py-4">
-      <h1 className="text-3xl font-bold text-center text-gray-800">
+    <header className="py-3 border-b border-gray-300 w-full relative">
+      <h1 className="text-2xl font-semibold text-center text-gray-800">
         ConnectionWords
       </h1>
+      {/* İnce çizgi: Gridin sol üst ve sağ üst kutularının hizasına denk gelecek şekilde */}
+      <div className="mt-2 mx-auto" style={{width: '31rem', height: '1px', backgroundColor: '#ccc'}}></div>
     </header>
   );
 };
 
 export default Header;
+
 
 
 
